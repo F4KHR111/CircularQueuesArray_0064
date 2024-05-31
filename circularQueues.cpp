83d25620fe52f7d0ba82fd63c2164ad2fb51ee65
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-05-31 14:36:48 +0700

    Menambah beberapa member di dalam Method insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -1,4 +1,4 @@
-#include <iosream>
+#include <iostream>
 using namespace std;
 
 class Queues{
@@ -11,6 +11,15 @@ public:
     }
 
     void insert(){
-        
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // Cek apakah antrian penuh
+        if((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
     }
 };
